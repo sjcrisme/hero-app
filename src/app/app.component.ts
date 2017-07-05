77a2b482ea7dcf4024cc8b526ea9739cc101cb60
Serhii Verbitskiy <sjcris.me@gmail.com>
2017-07-05 18:14:38 +0300

    first commit hero app

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component,VERSION,OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
+})
+
+export class AppComponent {
+
+  title = 'app';
+  angularTitle = `Angular v${VERSION.full}`;
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+ 
+export let HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice',text:"some text" },
+    { id: 12, name: 'Narco',text:"some text" },
+    { id: 13, name: 'Bombasto',text:"some text" },
+    { id: 14, name: 'Celeritas',text:"some text" },
+    { id: 15, name: 'Magneta',text:"some text" },
+    { id: 16, name: 'RubberMan',text:"some text" },
+    { id: 17, name: 'Dynama',text:"some text" },
+    { id: 18, name: 'Dr IQ',text:"some text" },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
